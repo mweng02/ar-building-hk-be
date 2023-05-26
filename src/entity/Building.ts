76ef37ce25eb4224f2427cded2044344c3e223a9
mweng02 <mweng02@gmail.com>
2023-05-27 00:40:18 +0800

    Update Schema

--- src/entity/Building.ts
+++ src/entity/Building.ts
@@ -3,8 +3,9 @@ import { Entity, Column, BaseEntity, Index, PrimaryGeneratedColumn } from "typeo
 @Entity({engine: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin '})
 @Index(["id"], { unique: true }) 
 @Index(["building_name_en"], { unique: true })
+@Index(["estate_name_en"], { unique: true })
 @Index(["building_name_zh"])
-@Index(["address"])
+@Index(["district","address","year_built","no_of_basement","no_of_storey"])
 @Index(["latitude", "longitude"], { unique: true })
 export class Building extends BaseEntity  {
     @PrimaryGeneratedColumn('increment')
@@ -13,11 +14,29 @@ export class Building extends BaseEntity  {
     @Column({length: 1111})
     building_name_en: string
 
+    @Column({length: 1111})
+    estate_name_en: string
+
     @Column({length: 1111, nullable: true, charset: 'utf8mb4', collation: 'utf8mb4_unicode_ci'})
     building_name_zh: string
 
+    @Column({length: 1111})
+    district: string
+
     @Column({length: 1111, nullable: true})
     address: string
+
+    @Column({length: 1111})
+    year_built: string
+
+    @Column({length: 1111})
+    no_of_units: string
+
+    @Column({length: 1111})
+    no_of_storey: string
+
+    @Column({length: 1111})
+    no_of_basement: string
 
     @Column({type:'longtext', nullable: true})
     building_description_en: string
